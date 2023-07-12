ec18edb8f980811327cddc405f7a0daa1136b262
Denis Ivanchenko <sicknature24@yandex.com>
2023-07-12 14:35:19 +0300

    feat: add ts, eslint, and prettier to the project

--- async-race/src/index.js
+++
@@ -1,8 +0,0 @@
-import "./index.html";
-import "./index.scss";
-
-const div = document.createElement("div");
-div.classList.add("test-div");
-div.innerText = "test message";
-
-document.body.appendChild(div);
--- async-race/webpack.config.js
+++ async-race/webpack.config.js
@@ -1,48 +1,56 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const environment = process.env.NODE_ENV || "development";
-const isDevelopmentEnvironment = environment === "development";
-const sourceMapType = isDevelopmentEnvironment ? "source-map" : undefined;
+const environment = process.env.NODE_ENV || 'development';
+const isDevelopmentEnvironment = environment === 'development';
+const sourceMapType = isDevelopmentEnvironment ? 'source-map' : undefined;
 
 module.exports = {
-  mode: environment,
-  devtool: sourceMapType,
-  devServer: {
-    open: true,
-    hot: true,
-  },
-  entry: path.resolve(__dirname, "src", "index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    filename: "index.js",
-    assetModuleFilename: "assets",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
-    }),
-    new MiniCssExtractPlugin({ filename: "styles.css" }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(c|sa|sc)ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/[name][ext]",
-        },
-      },
+    mode: environment,
+    devtool: sourceMapType,
+    devServer: {
+        open: true,
+        hot: true,
+    },
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: 'index.js',
+        assetModuleFilename: 'assets',
+    },
+    resolve: {
+        extensions: ['.js', '.ts'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+        new MiniCssExtractPlugin({ filename: 'styles.css' }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/[name][ext]',
+                },
+            },
+            {
+                test: /\.ts$/i,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
 };
