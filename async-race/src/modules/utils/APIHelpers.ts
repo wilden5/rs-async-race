fc28446b27ae79f903397b75342ad76ec1f1cf40
Denis Ivanchenko <sicknature24@yandex.com>
2023-07-13 13:52:24 +0300

    feat: add helper classes for DOM, API actions

---
+++ async-race/src/modules/utils/APIHelpers.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export async function getNumberOfCarsInGarage(): Promise<number> {
+    const response = await fetch('http://127.0.0.1:3000/garage');
+    const data: number[] = await response.json();
+    return data.length;
+}
