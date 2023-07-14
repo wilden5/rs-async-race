3e23d908c1fd0a665ca7675d6e06c80048605d0f
Denis Ivanchenko <sicknature24@yandex.com>
2023-07-14 22:04:20 +0300

    feat: add update car elements

--- async-race/src/modules/garage/GarageView.ts
+++ async-race/src/modules/garage/GarageView.ts
@@ -8,7 +8,7 @@ class GarageView {
 
     public GARAGE_PAGE: HTMLElement;
 
-    public ENTITY_CONTAINER: HTMLElement;
+    public CREATE_CONTAINER: HTMLElement;
 
     public CREATE_CAR_INPUT: HTMLElement;
 
@@ -16,31 +16,55 @@ class GarageView {
 
     public CREATE_CAR_BUTTON: HTMLElement;
 
+    public UPDATE_CONTAINER: HTMLElement;
+
+    public UPDATE_CAR_INPUT: HTMLElement;
+
+    public UPDATE_CAR_COLOR: HTMLElement;
+
+    public UPDATE_CAR_BUTTON: HTMLElement;
+
     public onCreateCarButtonClick: (name: string, color: string) => void = () => {};
 
     constructor(commonView: CommonView) {
         this.COMMON_VIEW = commonView;
         this.GARAGE_TITLE = DOMHelpers.createElement('div', ['garage__title'], 'Garage');
         this.GARAGE_PAGE = DOMHelpers.createElement('div', ['garage__page'], 'Page #1');
-        this.ENTITY_CONTAINER = DOMHelpers.createElement('div', ['entity__container']);
-        this.CREATE_CAR_INPUT = DOMHelpers.createElement('input', ['entity_create-car']);
-        this.SET_CAR_COLOR = DOMHelpers.createElement('input', ['entity_set-color']);
-        this.CREATE_CAR_BUTTON = DOMHelpers.createElement('button', ['entity_create-button', 'button'], 'Create car');
+        this.CREATE_CONTAINER = DOMHelpers.createElement('div', ['create-container']);
+        this.CREATE_CAR_INPUT = DOMHelpers.createElement('input', ['create-car']);
+        this.SET_CAR_COLOR = DOMHelpers.createElement('input', ['create-color']);
+        this.CREATE_CAR_BUTTON = DOMHelpers.createElement('button', ['create-button', 'button'], 'Create car');
+        this.UPDATE_CONTAINER = DOMHelpers.createElement('div', ['update-container']);
+        this.UPDATE_CAR_INPUT = DOMHelpers.createElement('input', ['update-car']);
+        this.UPDATE_CAR_COLOR = DOMHelpers.createElement('input', ['update-color']);
+        this.UPDATE_CAR_BUTTON = DOMHelpers.createElement('button', ['update-button', 'button'], 'Update car');
     }
 
     private appendElements(): void {
         DOMHelpers.appendChildToElement(this.COMMON_VIEW.GARAGE_CONTAINER, this.GARAGE_TITLE);
         DOMHelpers.appendChildToElement(this.COMMON_VIEW.GARAGE_CONTAINER, this.GARAGE_PAGE);
-        DOMHelpers.appendChildToElement(this.COMMON_VIEW.NAVIGATION_CONTAINER, this.ENTITY_CONTAINER);
-        DOMHelpers.appendChildToElement(this.ENTITY_CONTAINER, this.CREATE_CAR_INPUT);
-        DOMHelpers.appendChildToElement(this.ENTITY_CONTAINER, this.SET_CAR_COLOR);
-        DOMHelpers.appendChildToElement(this.ENTITY_CONTAINER, this.CREATE_CAR_BUTTON);
+        DOMHelpers.appendChildToElement(this.COMMON_VIEW.NAVIGATION_CONTAINER, this.CREATE_CONTAINER);
+        DOMHelpers.appendChildToElement(this.COMMON_VIEW.NAVIGATION_CONTAINER, this.UPDATE_CONTAINER);
+        DOMHelpers.appendChildToElement(this.CREATE_CONTAINER, this.CREATE_CAR_INPUT);
+        DOMHelpers.appendChildToElement(this.CREATE_CONTAINER, this.SET_CAR_COLOR);
+        DOMHelpers.appendChildToElement(this.CREATE_CONTAINER, this.CREATE_CAR_BUTTON);
+        DOMHelpers.appendChildToElement(this.UPDATE_CONTAINER, this.UPDATE_CAR_INPUT);
+        DOMHelpers.appendChildToElement(this.UPDATE_CONTAINER, this.UPDATE_CAR_COLOR);
+        DOMHelpers.appendChildToElement(this.UPDATE_CONTAINER, this.UPDATE_CAR_BUTTON);
     }
 
     private setUpInputElements(): void {
         this.CREATE_CAR_INPUT.setAttribute('type', 'text');
-        this.CREATE_CAR_INPUT.setAttribute('placeholder', 'Enter car name here');
+        this.CREATE_CAR_INPUT.setAttribute('placeholder', 'New car name');
         this.SET_CAR_COLOR.setAttribute('type', 'color');
+
+        this.UPDATE_CAR_INPUT.setAttribute('type', 'text');
+        this.UPDATE_CAR_INPUT.setAttribute('placeholder', 'Update existing car name');
+        this.UPDATE_CAR_COLOR.setAttribute('type', 'color');
+
+        (this.UPDATE_CAR_INPUT as HTMLInputElement).disabled = true;
+        (this.UPDATE_CAR_COLOR as HTMLInputElement).disabled = true;
+        (this.UPDATE_CAR_BUTTON as HTMLButtonElement).disabled = true;
     }
 
     public updateGarageTitle(numberOfCars: number): void {
