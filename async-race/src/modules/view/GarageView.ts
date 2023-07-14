312c9319bf7842bd10c01f5551541d21da897d6a
Denis Ivanchenko <sicknature24@yandex.com>
2023-07-14 13:00:54 +0300

    feat: add new garage elements

--- async-race/src/modules/view/GarageView.ts
+++ async-race/src/modules/view/GarageView.ts
@@ -8,15 +8,36 @@ class GarageView {
 
     public GARAGE_PAGE: HTMLElement;
 
+    public ENTITY_CONTAINER: HTMLElement;
+
+    public CREATE_CAR_INPUT: HTMLElement;
+
+    public SET_CAR_COLOR: HTMLElement;
+
+    public CREATE_CAR_BUTTON: HTMLElement;
+
     constructor(commonView: CommonView) {
         this.COMMON_VIEW = commonView;
         this.GARAGE_TITLE = DOMHelpers.createElement('div', ['garage__title'], 'Garage');
         this.GARAGE_PAGE = DOMHelpers.createElement('div', ['garage__page'], 'Page #1');
+        this.ENTITY_CONTAINER = DOMHelpers.createElement('div', ['entity__container']);
+        this.CREATE_CAR_INPUT = DOMHelpers.createElement('input', ['entity_create-car']);
+        this.SET_CAR_COLOR = DOMHelpers.createElement('input', ['entity_set-color']);
+        this.CREATE_CAR_BUTTON = DOMHelpers.createElement('button', ['entity_create-button', 'button'], 'Create car');
     }
 
     private appendElements(): void {
         DOMHelpers.appendChildToElement(this.COMMON_VIEW.GARAGE_CONTAINER, this.GARAGE_TITLE);
         DOMHelpers.appendChildToElement(this.COMMON_VIEW.GARAGE_CONTAINER, this.GARAGE_PAGE);
+        DOMHelpers.appendChildToElement(this.COMMON_VIEW.NAVIGATION_CONTAINER, this.ENTITY_CONTAINER);
+        DOMHelpers.appendChildToElement(this.ENTITY_CONTAINER, this.CREATE_CAR_INPUT);
+        DOMHelpers.appendChildToElement(this.ENTITY_CONTAINER, this.SET_CAR_COLOR);
+        DOMHelpers.appendChildToElement(this.ENTITY_CONTAINER, this.CREATE_CAR_BUTTON);
+    }
+
+    private setUpInputElements(): void {
+        this.CREATE_CAR_INPUT.setAttribute('type', 'text');
+        this.SET_CAR_COLOR.setAttribute('type', 'color');
     }
 
     public updateGarageTitle(numberOfCars: number): void {
@@ -25,6 +46,7 @@ class GarageView {
 
     public setupDOMElementsAndEventHandlers(): void {
         this.appendElements();
+        this.setUpInputElements();
     }
 }
 
