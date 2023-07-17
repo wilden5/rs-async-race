b1abb45ddf04be86b067d3ce2523250615905125
Denis Ivanchenko <sicknature24@yandex.com>
2023-07-17 23:20:03 +0300

    feat: add simple footer

--- async-race/src/modules/common/CommonView.ts
+++ async-race/src/modules/common/CommonView.ts
@@ -1,4 +1,5 @@
 import DOMHelpers from '../../utils/DOMHelpers';
+import Constants from '../../utils/Constants';
 
 class CommonView {
     public WRAPPER: HTMLElement;
@@ -13,6 +14,14 @@ class CommonView {
 
     public WINNERS_CONTAINER: HTMLElement;
 
+    public FOOTER_CONTAINER: HTMLElement;
+
+    public FOOTER_AUTHOR: HTMLElement;
+
+    public FOOTER_SCHOOL: HTMLElement;
+
+    public FOOTER_SCHOOL_IMG: HTMLElement;
+
     constructor() {
         this.WRAPPER = DOMHelpers.createElement('div', ['wrapper']);
         this.NAVIGATION_CONTAINER = DOMHelpers.createElement('div', ['navigation-panel-container']);
@@ -20,6 +29,10 @@ class CommonView {
         this.WINNERS_BUTTON = DOMHelpers.createElement('button', ['winners-button', 'button'], 'Check Winners');
         this.GARAGE_CONTAINER = DOMHelpers.createElement('div', ['garage-container']);
         this.WINNERS_CONTAINER = DOMHelpers.createElement('div', ['winners-container', 'disabled']);
+        this.FOOTER_CONTAINER = DOMHelpers.createElement('div', ['footer-container']);
+        this.FOOTER_AUTHOR = DOMHelpers.createElement('a', ['footer-author'], '🥴wilden5');
+        this.FOOTER_SCHOOL = DOMHelpers.createElement('a', ['footer-school']);
+        this.FOOTER_SCHOOL_IMG = DOMHelpers.createElement('img', ['footer-school-img']);
     }
 
     private appendElements(): void {
@@ -29,6 +42,10 @@ class CommonView {
         DOMHelpers.appendChildToElement(this.NAVIGATION_CONTAINER, this.WINNERS_BUTTON);
         DOMHelpers.appendChildToElement(this.WRAPPER, this.GARAGE_CONTAINER);
         DOMHelpers.appendChildToElement(this.WRAPPER, this.WINNERS_CONTAINER);
+        DOMHelpers.appendChildToElement(this.WRAPPER, this.FOOTER_CONTAINER);
+        DOMHelpers.appendChildToElement(this.FOOTER_CONTAINER, this.FOOTER_AUTHOR);
+        DOMHelpers.appendChildToElement(this.FOOTER_CONTAINER, this.FOOTER_SCHOOL);
+        DOMHelpers.appendChildToElement(this.FOOTER_SCHOOL, this.FOOTER_SCHOOL_IMG);
     }
 
     private handleGarageButtonClick = (): void => {
@@ -41,8 +58,20 @@ class CommonView {
         this.WINNERS_CONTAINER.classList.remove('disabled');
     };
 
+    private setFooterLinks = (): void => {
+        (this.FOOTER_AUTHOR as HTMLLinkElement).setAttribute('href', Constants.WILDEN_GITHUB_LINK);
+        (this.FOOTER_AUTHOR as HTMLLinkElement).setAttribute('target', '_blank');
+
+        (this.FOOTER_SCHOOL as HTMLLinkElement).setAttribute('href', Constants.RS_SCHOOL_LINK);
+        (this.FOOTER_SCHOOL as HTMLLinkElement).setAttribute('target', '_blank');
+
+        (this.FOOTER_SCHOOL_IMG as HTMLImageElement).setAttribute('src', Constants.RS_SCHOOL_IMG_LINK);
+        (this.FOOTER_SCHOOL_IMG as HTMLImageElement).setAttribute('alt', 'rs-school-image');
+    };
+
     public setupDOMElementsAndEventHandlers(): void {
         this.appendElements();
+        this.setFooterLinks();
         this.GARAGE_BUTTON.addEventListener('click', this.handleGarageButtonClick);
         this.WINNERS_BUTTON.addEventListener('click', this.handleWinnersButtonClick);
     }
